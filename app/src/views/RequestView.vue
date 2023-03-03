c1df700f4975e2617fba0ac86cd6cae09b2dd28a
jwgerlach00 <jwgerlach00@gmail.com>
2023-03-03 13:14:01 -0500

    RequestView w/ UniProt ID input saving to a v-list

---
+++ app/src/views/RequestView.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <h1>RequestView</h1>
+    <RequestCard/>
+  </v-container>
+</template>
+
+<script setup>
+import RequestCard from '@/components/RequestCard.vue'
+</script>
