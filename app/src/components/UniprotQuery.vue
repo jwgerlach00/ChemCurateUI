fe00ec2861aabd30419121f0a1a9a83e9eb681b2
jwgerlach00 <jwgerlach00@gmail.com>
2023-03-10 17:04:43 -0500

    Broken

--- app/src/components/UniprotQuery.vue
+++ app/src/components/UniprotQuery.vue
@@ -6,17 +6,23 @@
           v-model:search="organismSearch" color="black" chips multiple hide-details clearable/>
       </v-col>
     </v-row>
-    <v-row dense>
-      <v-col cols="12">
-        <v-autocomplete v-if="organismSelect.length" label="Proteins" placeholder="Type to reveal selections"
-          :loading="proteinLoading" :items="proteinItems" v-model="proteinSelect" v-model:search="proteinSearch"
-          color="black" chips multiple hide-details clearable>
-          <template v-slot:append-item>
-            <div v-intersect="endIntersect"></div>
-          </template>
-        </v-autocomplete>
-      </v-col>
-    </v-row>
+
+
+    <div v-if="organismSelect.length">
+      <v-row v-for="organism in organismSelect" dense :key="organism">
+        <v-col cols="12">
+          <v-autocomplete :label="`${organism} Proteins`" placeholder="Type to reveal selections"
+            :loading="proteinLoading" :items="proteinItems[organism]" v-model="proteinSelect[organism]"
+            v-model:search="proteinSearch[organism]" color="black" chips multiple hide-details clearable>
+            <template v-slot:append-item>
+              <div v-intersect="endIntersect"></div>
+            </template>
+          </v-autocomplete>
+        </v-col>
+      </v-row>
+    </div>
+
+
     <v-row dense>
       <v-col cols="12">
         <v-btn v-if="proteinSelect.length" color="green-accent-2" block>Submit</v-btn>
@@ -41,13 +47,14 @@ const organismSearch = ref(null)
 const organismSelect = ref([])
 
 const proteinLoading = ref(false)
-const proteinItems = ref([])
-const proteinSearch = ref(null)
-const proteinSelect = ref([])
+const proteinItems = ref({})
+const proteinSearch = ref({})
+const proteinSelect = ref({})
 
 function getUniprotMap () {
   axios.get(`${API_URL}/get_uniprot_map`).then((res) => {
     uniprotMapObject = res.data
+    console.log(res.data)
   }).catch((error) => {
     console.log(error)
   })
@@ -55,7 +62,7 @@ function getUniprotMap () {
 
 function endIntersect (entries, observer, isIntersecting) {
   if (isIntersecting) {
-    proteinItems.value = queryFilter(Object.keys(uniprotMapObject[organismSelect.value[0]]['protein']), 
+    proteinItems.value = queryFilter(uniprotMapObject[organismSelect.value[0]], 
       proteinSearch.value).slice(0, proteinItems.value.length + 10)
   }
 }
@@ -74,13 +81,27 @@ function queryFilter(items, value) {
   })
 }
 
+watch(organismSelect, (val) => {
+  for (let organism of val) {
+    // li = li.concat(uniprotMapObject[organismSelect.value[organism]])
+    proteinSelect.value[organism] = []
+    proteinSearch.value[organism] = null
+    console.log(uniprotMapObject[organism])
+    console.log(queryFilter(uniprotMapObject[organism], val).slice(0, 10))
+    // proteinItems.value[organism] = queryFilter(uniprotMapObject[organism], val).slice(0, 10)
+  }
+})
+
 watch(organismSearch, (val) => {
   val && val !== organismSelect && queryOrganismSelections(val)
 })
 
-watch(proteinSearch, (val) => {
-  val && val !== proteinSelect && queryProteinSelections(val)
-})
+for (let organism in organismSelect.value) {
+  watch(proteinSearch[organism], (val) => {
+    val && val !== proteinSelect && queryProteinSelections(val, organism)
+  })
+  console.log(organism)
+}
 
 function queryOrganismSelections (val) {
   organismLoading.value = true
@@ -88,9 +109,9 @@ function queryOrganismSelections (val) {
   organismLoading.value = false
 }
 
-function queryProteinSelections (val) {
+function queryProteinSelections (val, organism) {
   proteinLoading.value = true
-  proteinItems.value = queryFilter(Object.keys(uniprotMapObject[organismSelect.value[0]]['protein']), val).slice(0, 10)
+  // proteinItems.value = queryFilter(uniprotMapObject[organismSelect.value[organism]], val).slice(0, 10)
   proteinLoading.value = false
 }
 </script>
